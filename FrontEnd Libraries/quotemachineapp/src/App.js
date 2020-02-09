a966b792078df98785f264edae5c9992356af1b1
Nick.420 <ddjnick03@gmail.com>
2020-02-09 03:55:00 +0100

    Update App.js

--- FrontEnd Libraries/quotemachineapp/src/App.js
+++ FrontEnd Libraries/quotemachineapp/src/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
     }
     this.changeCit = this.changeCit.bind(this)
   }
+  
   changeCit() {
     const index = Math.floor(Math.random() * (+10 - +1)) + +1; 
     this.setState({
@@ -17,10 +18,11 @@ class App extends React.Component {
       author : text.frase[index].man
     })
   }
-  //Body/Button/Text/
+
   render() {
     const colors = ['#2980b9', '#e74c3c', '#8e44ad'];
     let random_color = colors[Math.floor(Math.random() * colors.length)];
+    
     const myStyle = {
       color: random_color,
       transitionDuration: 0.4
@@ -28,16 +30,17 @@ class App extends React.Component {
     const headStyle = {
       backgroundColor: random_color,
     }
+    
     return (
       <div className="App">
         <header style={headStyle} className="App-header">
           <h1>My custom script for FreeCodeCamp!</h1>
             <div id="quote-box">
-            <h2 className="myText" id="text" style={myStyle}><cite>"{this.state.text}</cite></h2>
-            <h5 className="myText" id="author" style={myStyle}>{this.state.author}</h5>
-            <a href="tweet"><button style={headStyle} className="new-quote" >Tweet</button></a>
-<button style={headStyle} className="new-quote" onClick={this.changeCit}>Press ME!</button>
-            </div>
+              <h2 className="myText" id="text" style={myStyle}><cite>"{this.state.text}</cite></h2>
+              <h5 className="myText" id="author" style={myStyle}>{this.state.author}</h5>
+              <a href="tweet"><button style={headStyle} className="new-quote" >Tweet</button></a>
+              <button style={headStyle} className="new-quote" onClick={this.changeCit}>Press ME!</button>
+             </div>
             <div id="footer">
               <p>Created by Nick</p>
             </div>
